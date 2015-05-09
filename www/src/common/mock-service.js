734ff7c54c30d56efd45b007736ae58e00b48f12
Jossef Harush <jossef12@gmail.com>
2015-05-09 22:17:29 +0300

    changed contacts to match the init-data script

--- www/src/common/mock-service.js
+++ www/src/common/mock-service.js
@@ -12,174 +12,30 @@
             pickContact: pickContact
         };
 
-        function pickContact(){
-            var item = _contacts[Math.floor(Math.random()*_contacts.length)];
-
-            var email;
-
-            if (item.emails && item.emails.length)
-            {
-                email = item.emails[0].value;
-            }
-
-            var phone;
-
-            if (item.phoneNumbers && item.phoneNumbers.length)
-            {
-                phone = item.phoneNumbers[0].value;
-            }
-
+        function pickContact() {
+            var item = _contacts[Math.floor(Math.random() * _contacts.length)];
             return {
-                name: item.displayName,
-                email: email,
-                phone: phone
+                name: item.name,
+                email: item.email,
+                phone: item.phone
             };
         }
 
     }
 
+
     var _contacts = [
-        {
-            "addresses": null,
-            "birthday": null,
-            "categories": null,
-            "displayName": "טבת באביר",
-            "emails": null,
-            "id": "434",
-            "ims": null,
-            "name": {
-                "familyName": "באביר",
-                "formatted": "טבת באביר",
-                "givenName": "טבת"
-            },
-            "nickname": null,
-            "note": "",
-            "organizations": null,
-            "phoneNumbers": [
-                {
-                    "id": "3091",
-                    "pref": false,
-                    "type": "mobile",
-                    "value": "0545551189"
-                }
-            ],
-            "photos": null,
-            "rawId": "432",
-            "urls": null
-        },
-        {
-            "addresses": null,
-            "birthday": null,
-            "categories": null,
-            "displayName": "Nir Gabbre",
-            "emails": [
-                {
-                    "id": "4441",
-                    "pref": false,
-                    "type": "other",
-                    "value": "Nir@gmail.com"
-                }
-            ],
-            "id": "614",
-            "ims": null,
-            "name": {
-                "familyName": "Gabbre",
-                "formatted": "Nir Gabbre",
-                "givenName": "Nir"
-            },
-            "nickname": null,
-            "note": "",
-            "organizations": null,
-            "phoneNumbers": null,
-            "photos": null,
-            "rawId": "611",
-            "urls": null
-        },
-        {
-            "addresses": null,
-            "birthday": null,
-            "categories": null,
-            "displayName": "The Papercut Factory",
-            "emails": null,
-            "id": "600",
-            "ims": null,
-            "name": {
-                "familyName": "Factory",
-                "formatted": "The Papercut Factory",
-                "givenName": "The",
-                "middleName": "Papercut"
-            },
-            "nickname": null,
-            "note": "",
-            "organizations": null,
-            "phoneNumbers": [
-                {
-                    "id": "4315",
-                    "pref": false,
-                    "type": "mobile",
-                    "value": "036007997"
-                }
-            ],
-            "photos": null,
-            "rawId": "593",
-            "urls": null
-        },
-        {
-            "addresses": null,
-            "birthday": null,
-            "categories": null,
-            "displayName": "ארז דורי",
-            "emails": null,
-            "id": "435",
-            "ims": null,
-            "name": {
-                "familyName": "דורי",
-                "formatted": "ארז דורי",
-                "givenName": "ארז"
-            },
-            "nickname": null,
-            "note": "",
-            "organizations": null,
-            "phoneNumbers": [
-                {
-                    "id": "3152",
-                    "pref": false,
-                    "type": "mobile",
-                    "value": "0545516444"
-                }
-            ],
-            "photos": null,
-            "rawId": "440",
-            "urls": null
-        },
-        {
-            "addresses": null,
-            "birthday": null,
-            "categories": null,
-            "displayName": "ירון הראל",
-            "emails": null,
-            "id": "436",
-            "ims": null,
-            "name": {
-                "familyName": "הראל",
-                "formatted": "ירון הראל",
-                "givenName": "ירון"
-            },
-            "nickname": null,
-            "note": "",
-            "organizations": null,
-            "phoneNumbers": [
-                {
-                    "id": "3029",
-                    "pref": false,
-                    "type": "mobile",
-                    "value": "0505298872"
-                }
-            ],
-            "photos": null,
-            "rawId": "424",
-            "urls": null
-        }
+        {phone: '0505500010', name: 'Jossef', email: 'jossef@gmail.com'},
+        {phone: '0505500011', name: 'Moshe', email: 'moshe@gmail.com'},
+        {phone: '0505500012', name: 'Aharon', email: 'aharon@gmail.com'},
+        {phone: '0505500013', name: 'David', email: 'david@gmail.com'},
+        {phone: '0505500014', name: 'Lior', email: 'lior@gmail.com'},
+        {phone: '0505500015', name: 'Reut', email: 'reut@gmail.com'},
+        {phone: '0505500016', name: 'Adi', email: 'adi@gmail.com'},
+        {phone: '0505500017', name: 'Liat', email: 'liat@gmail.com'},
+        {phone: '0505500018', name: 'Osnat', email: 'osnat@gmail.com'},
+        {phone: '0505500019', name: 'Naama', email: 'naama@gmail.com'},
+        {phone: '0505500020', name: 'Lizi', email: 'lizi@gmail.com'}
     ];
 
 
